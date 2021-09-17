b4169fdfb804d250b4d9e95a5aa78618d92061d7
Lzadhito <lzadhito@pop-os.localdomain>
2021-09-17 22:29:49 +0700

    refactor(/): add reusable auto cache Fetcher helper

--- lib/screens/Home/services/curriculum_service.dart
+++ lib/screens/Home/services/curriculum_service.dart
@@ -1,18 +1,11 @@
-import 'package:dio/dio.dart';
-import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:hijra_steps/screens/Home/models/Level.dart';
+import 'package:hijra_steps/screens/helpers/fetcher.dart';
 
 class CurriculumService {
   Future<List<Level>> fetchCurriculum() async {
-    DioCacheManager _dioCacheManager = DioCacheManager(CacheConfig());
-    Options _cacheOptions =
-        buildCacheOptions(Duration(days: 7), forceRefresh: true);
-    Dio _dio = Dio();
-    _dio.interceptors.add(_dioCacheManager.interceptor);
-
     try {
       const String url = "http://192.168.1.13:3000/curriculum";
-      final response = await _dio.get(url, options: _cacheOptions);
+      final response = await Fetcher().fetch(url);
 
       if (response.statusCode == 200) {
         final jsonData = response.data;
--- lib/screens/Home/services/profile_service.dart
+++ lib/screens/Home/services/profile_service.dart
@@ -1,18 +1,11 @@
-import 'package:dio/dio.dart';
-import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:hijra_steps/screens/Home/models/Profile.dart';
+import 'package:hijra_steps/screens/helpers/fetcher.dart';
 
 class ProfileService {
   Future<Profile> fetchProfile() async {
-    DioCacheManager _dioCacheManager = DioCacheManager(CacheConfig());
-    Options _cacheOptions =
-        buildCacheOptions(Duration(days: 7), forceRefresh: true);
-    Dio _dio = Dio();
-    _dio.interceptors.add(_dioCacheManager.interceptor);
-
     try {
       const String url = "http://192.168.1.13:3000/profile";
-      final response = await _dio.get(url, options: _cacheOptions);
+      final response = await Fetcher().fetch(url);
 
       if (response.statusCode == 200) {
         return Profile.fromJson(response.data);

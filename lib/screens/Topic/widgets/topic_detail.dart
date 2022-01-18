638047ae45472ef43be7bdecbb4d4e715d4ec72b
Lzadhito <Lzadhito@gmail.com>
2022-01-18 22:08:37 +0700

    feat(topic): center description & use card

--- lib/screens/Topic/widgets/topic_detail.dart
+++ lib/screens/Topic/widgets/topic_detail.dart
@@ -26,22 +26,22 @@ class TopicDetail extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Title & Author
           Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            padding:
+                const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: Text(title,
-                      style: Theme.of(context)
-                          .textTheme
-                          .headline2!
-                          .copyWith(color: primaryGreen)),
-                ),
+                Text(title,
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline2!
+                        .copyWith(color: primaryGreen)),
                 Padding(
                   padding: const EdgeInsets.only(top: 8.0),
                   child: Text(
@@ -53,19 +53,18 @@ class TopicDetail extends StatelessWidget {
             ),
           ),
           // Description
-          Container(
-              margin: const EdgeInsets.symmetric(vertical: 16),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border:
-                      Border.all(color: const Color(0XFF403F3F), width: 0.5)),
+          if (desc != '')
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15)),
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16),
+                padding: const EdgeInsets.only(
+                    top: 12, left: 12, right: 12, bottom: 48),
                 child: Html(data: desc),
-              )),
+              ),
+            ),
           Container(
-              margin: const EdgeInsets.only(bottom: 24.0),
+              margin: const EdgeInsets.symmetric(vertical: 24.0),
               width: double.infinity,
               child: UnifyButton(
                 onPress: () => onPressSelesai(context),
